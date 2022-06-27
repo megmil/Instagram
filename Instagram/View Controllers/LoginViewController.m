c86c8af389fa4320f2a26335650496e05269292d
megmil <megmil@umich.edu>
2022-06-27 14:44:14 -0700

    Implemented signup/login

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -6,8 +6,14 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
 
 @end
 
@@ -15,17 +21,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self.loginButton setTitle:@"Log in" forState:UIControlStateNormal];
+    [self.loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)showAlert {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Missing Text Field(s)"
+                                                                   message:@"Please fill in username and password."
+                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"Ok"
+                                                     style:UIAlertActionStyleCancel
+                                                   handler:^(UIAlertAction * _Nonnull action) {
+    }];
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:^{
+        return;
+    }];
 }
-*/
+
+- (IBAction)loginUser:(id)sender {
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        [self showAlert];
+    } else {
+        NSString *username = self.usernameField.text;
+        NSString *password = self.passwordField.text;
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User logged in successfully");
+                [self performSegueWithIdentifier:@"tabSegue" sender:self];
+            }
+        }];
+    }
+}
+
+- (IBAction)registerUser:(id)sender {
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        [self showAlert];
+    } else {
+        PFUser *newUser = [PFUser user];
+        newUser.username = self.usernameField.text;
+        newUser.password = self.passwordField.text;
+            
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User registered successfully");
+                [self performSegueWithIdentifier:@"tabSegue" sender:self];
+            }
+        }];
+    }
+}
 
 @end
