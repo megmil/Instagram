62240fb0f7b248227f43eb6317187b49254ddb35
megmil <megmil@umich.edu>
2022-06-27 15:52:16 -0700

    Implemented image post.

--- Instagram/View Controllers/ComposeViewController.m
+++ Instagram/View Controllers/ComposeViewController.m
@@ -6,8 +6,12 @@
 //
 
 #import "ComposeViewController.h"
+#import "Post.h"
 
-@interface ComposeViewController ()
+@interface ComposeViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *captionField;
 
 @end
 
@@ -15,17 +19,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)clear {
+    UIImage *placeholderImage = [UIImage imageNamed:@"image_placeholder"];
+    [self.imageView setImage:placeholderImage];
+    [self.captionField setText:@""];
 }
-*/
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage]; // TODO: remove?
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    [self.imageView setImage:editedImage];
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)didTapImageView:(id)sender {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (IBAction)cancel:(id)sender {
+    [self clear];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)share:(id)sender {
+    // TODO: share action
+    [Post postUserImage:[self.imageView image] withCaption:[self.captionField text] withCompletion:nil];
+    [self clear];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
