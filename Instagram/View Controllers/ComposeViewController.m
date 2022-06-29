06d8e52a9247df825f1b38e9b2c0a2e8a56a60c5
megmil <megmil@umich.edu>
2022-06-29 15:34:11 -0700

    Added progress bar

--- Instagram/View Controllers/ComposeViewController.m
+++ Instagram/View Controllers/ComposeViewController.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UITextField *captionField;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
+@property (weak, nonatomic) IBOutlet UILabel *progressLabel;
 
 @end
 
@@ -19,20 +21,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self hideProgress];
 }
 
 - (void)clear {
     UIImage *placeholderImage = [UIImage imageNamed:@"image_placeholder"];
     [self.imageView setImage:placeholderImage];
     [self.captionField setText:@""];
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
-
-- (void)imagePickerController:(UIImagePickerController *)picker
-didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
-    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
-    [self.imageView setImage:editedImage];
+    [self hideProgress];
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -55,8 +51,47 @@ didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
 }
 
 - (IBAction)share:(id)sender {
-    [Post postUserImage:[self.imageView image] withCaption:[self.captionField text] withCompletion:nil];
-    [self clear];
+    [self startProgress];
+    
+    [Post postUserImage:[self.imageView image] withCaption:[self.captionField text] withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
+        if (succeeded) {
+            [self endProgress];
+        } else {
+            [self clear];
+        }
+    }];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    [self.imageView setImage:editedImage];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)hideProgress {
+    [self.progressView setHidden:YES];
+    [self.progressLabel setHidden:YES];
+}
+
+- (void)startProgress {
+    [self.progressView setHidden:NO];
+    [self.progressLabel setHidden:NO];
+    [self animateProgressBarWithDuration:1.7];
+}
+
+- (void)endProgress {
+    [self.progressView setProgress:1 animated:YES];
+    [self.progressLabel setHidden:YES];
+    [self performSelector:@selector(clear) withObject:self afterDelay:0.7];
+}
+
+- (void)animateProgressBarWithDuration:(CGFloat)duration {
+    [self.progressView setProgress:0.0];
+    [UIView animateWithDuration:duration animations:^{
+        [self.progressView setProgress:0.7 animated:YES];
+    }];
 }
 
 @end
