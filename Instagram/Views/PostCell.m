3c50f5884eb9b87508bc9a147f14f69a44c7f47c
megmil <megmil@umich.edu>
2022-06-29 10:36:08 -0700

    Implemented user profiles

--- Instagram/Views/PostCell.m
+++ Instagram/Views/PostCell.m
@@ -7,6 +7,10 @@
 
 #import "PostCell.h"
 
+@interface PostCell () <UIGestureRecognizerDelegate>
+
+@end
+
 @implementation PostCell
 
 - (void)awakeFromNib {
@@ -18,11 +22,30 @@
     
     [self.post.author fetchIfNeeded];
     
+    UITapGestureRecognizer *tapUsername = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showProfile)];
+    tapUsername.delegate = self;
+    [self.usernameLabel addGestureRecognizer:tapUsername];
+    
+    UITapGestureRecognizer *tapUserProfile = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showProfile)];
+    tapUserProfile.delegate = self;
+    [self.userImageView addGestureRecognizer:tapUserProfile];
+    
     PFFileObject *imageData = self.post.image;
     [imageData getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
         if (data) {
             UIImage *postImage = [UIImage imageWithData:data];
             [self.postImageView setImage:postImage];
+        }
+        else {
+            NSLog(@"Unable to load image.");
+        }
+    }];
+    
+    PFFileObject *userImageData = [self.post.author valueForKey:@"profilePicture"];
+    [userImageData getDataInBackgroundWithBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+        if (data) {
+            UIImage *userImage = [UIImage imageWithData:data];
+            [self.userImageView setImage:userImage];
         }
         else {
             NSLog(@"Unable to load image.");
@@ -36,6 +59,11 @@
     
     [self.usernameLabel setText:self.post.author.username];
     [self.captionLabel setText:self.post.caption];
+}
+
+- (void)showProfile {
+    NSLog(@"tap");
+    [self.delegate didTapUser:self.post.author];
 }
 
 @end
